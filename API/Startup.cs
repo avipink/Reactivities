265ee120a0205b1169de51abf65a4829b1118dfc
Avi <avi@edea.co.il>
2020-06-04 18:47:16 +0300

    Implement Activity Query with mediator pattern

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -35,11 +37,15 @@ namespace API
             );
             services.AddCors(opt => {
                 opt.AddPolicy("CorsPolicy",policy => {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000","http://172.16.4.29:3000");
                 });
 
             });
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddControllers();
+            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
