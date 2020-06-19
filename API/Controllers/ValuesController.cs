2658602aa24610ada48c911d2bd753fe5218795a
avi <avi@edea.co.il>
2020-06-19 18:41:54 +0300

    Remove check station source code

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -50,11 +50,6 @@ namespace API.Controllers
         public void Delete(int id)
         {
         }
-
-         // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void stam(int id)
-        {
-        }
+       
     }
 }
