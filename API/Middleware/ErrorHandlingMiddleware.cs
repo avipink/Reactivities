5498240bddc1403e7ff6d24d3b244a89a8526f66
avi <avi@edea.co.il>
2020-07-31 16:04:44 +0300

    End of section 13

--- API/Middleware/ErrorHandlingMiddleware.cs
+++ API/Middleware/ErrorHandlingMiddleware.cs
@@ -38,14 +38,14 @@ namespace API.Middleware
                 case RestException re:
                     //logger.LogError(ex,"REST ERROR");
                     errors = re.Errors;
-                    logger.LogError(ex,"REST ERROR => " + errors.ToString()); 
+                    logger.LogError(ex,"REST ERROR => " + errors?.ToString()); 
                     context.Response.StatusCode = (int)re.Code; 
                     break;
                 
                 case Exception e:
                     //logger.LogError(ex,"SERVER ERROR");
                     errors = string.IsNullOrWhiteSpace(e.Message) ? "ERROR" : e.Message;
-                    logger.LogError(ex,"SERVER ERROR => " + errors.ToString());
+                    logger.LogError(ex,"SERVER ERROR => " + errors?.ToString());
                     context.Response.StatusCode = (int)HttpStatusCode.InternalServerError; 
                     break;
             }
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,13 +1,19 @@
-import { observable, action, computed, configure, runInAction } from "mobx";
-import { createContext, SyntheticEvent } from "react";
+import { observable, action, computed, runInAction } from "mobx";
+import { SyntheticEvent } from "react";
 import { IActivity, IActivityFormValues } from "../models/activity";
 import agent from "../api/agent";
 import { history } from "../..";
 import { toast } from "react-toastify";
+import { RootStore } from "./rootStore";
 
-configure({ enforceActions: "always" });
+export default class ActivityStore {
 
-class ActivityStore {
+  rootStore: RootStore;    
+
+  constructor(rootStore:RootStore){
+    this.rootStore = rootStore;
+  }
+
   @observable activitiesRegistry = new Map();
   @observable activity: IActivity | null = null;
   @observable loadingInitial = false;
@@ -162,4 +168,3 @@ class ActivityStore {
 
 }
 
-export default createContext(new ActivityStore());
